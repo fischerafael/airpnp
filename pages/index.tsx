2174ef058ffee1551647cefb3a49f5e096ba6890
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-03-18 22:18:11 -0300

    feat: handleSelect working

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,44 @@ import React, { useState } from "react";
 import { Header } from "../src/components/Header";
 import { DateRangePicker } from "react-date-range";
 
+const today = new Date();
+const rangeColors = ["#FD5B61"];
+
+interface ISelectRange {
+  startDate: Date;
+  endDate: Date;
+  key: string;
+}
+
 const HomePage = () => {
   const [dates, setDates] = useState({
     startDate: new Date(),
     endDate: new Date(),
   });
 
-  const selectionRange = {
+  console.log(dates);
+
+  const selectionRange: ISelectRange = {
     startDate: dates.startDate,
     endDate: dates.endDate,
-    key: "Selection",
+    key: "selection",
   };
+
+  function handleSelect(ranges: any) {
+    setDates({ ...dates, startDate: ranges.selection.startDate });
+    setDates({ ...dates, endDate: ranges.selection.endDate });
+  }
 
   return (
     <VStack w="full" minH="100vh">
       <Header />
 
-      <DateRangePicker ranges={[selectionRange]} />
+      <DateRangePicker
+        ranges={[selectionRange]}
+        minDate={today}
+        rangeColors={rangeColors}
+        onChange={handleSelect}
+      />
     </VStack>
   );
 };
