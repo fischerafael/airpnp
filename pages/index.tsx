ac414a5a8139c50d018ebbd0bce82c49ec5044d5
Rafael Fischer <rafaelsanfischer@gmail.com>
2022-03-18 22:08:49 -0300

    feat: add selection range

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
 import { Flex, HStack, Input, Text, VStack } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import { Header } from "../src/components/Header";
+import { DateRangePicker } from "react-date-range";
 
 const HomePage = () => {
+  const [dates, setDates] = useState({
+    startDate: new Date(),
+    endDate: new Date(),
+  });
+
+  const selectionRange = {
+    startDate: dates.startDate,
+    endDate: dates.endDate,
+    key: "Selection",
+  };
+
   return (
     <VStack w="full" minH="100vh">
       <Header />
+
+      <DateRangePicker ranges={[selectionRange]} />
     </VStack>
   );
 };
